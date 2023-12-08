b27478fd5f7b55bf40b354d7585196e56ec9fd5a
Deepanshu Goel <symaticvisuals02@gmail.com>
2023-12-09 01:18:02 +0530

    Added Common Components

---
+++ build/src/common/constants.js
@@ -0,0 +1,26 @@
+export const connectedNetwork = "ghostnet";
+export const rpcNode = "https://rpc.tzkt.io/ghostnet";
+
+
+export const teztrisContract = 'KT1FjNorFCBAxvWFK4k15nyiFiGBb4T12Gpx';
+
+export const ctez = {
+    betToken : "KT1Q4qRd8mKS7eWUgTfJzCN8RC6h9CzzjVJb",
+    betTokenId: 0,
+    betTokenType : "FA1.2",
+    betTokenDecimal : 6,
+}
+
+export const usdt = {
+    betToken : "KT1Uw1oio434UoWFuZTNKFgt5wTM9tfuf7m7",
+    betTokenId: 3,
+    betTokenType : "FA2",
+    betTokenDecimal : 6,
+}
+
+export const tez = {
+    betToken : "",
+    betTokenId: 0,
+    betTokenType : "tez",
+    betTokenDecimal : 6,
+}
