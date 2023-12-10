00dae64807fef025932119e0df6385d45c78ffa3
Deepanshu Goel <symaticvisuals02@gmail.com>
2023-12-10 07:28:31 +0530

    Backend Integration

--- build/src/main.jsx
+++ build/src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { init } from "@airstack/airstack-react";
+
+init(import.meta.env.VITE_AIRSTACK_API_KEY);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
